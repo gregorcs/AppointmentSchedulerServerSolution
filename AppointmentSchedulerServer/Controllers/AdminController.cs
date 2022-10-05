1e73917643ab124787cb4f4c1dd923eda6d06965
Gregor Csatlós <g.csatlos1@gmail.com>
2022-10-05 22:58:11 +0200

    Added custom requests to adminController

--- AppointmentSchedulerServer/Controllers/AdminController.cs
+++ AppointmentSchedulerServer/Controllers/AdminController.cs
@@ -1,83 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppointmentSchedulerServer.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppointmentSchedulerServer.Controllers
 {
+    [ApiController]
+    [Route("api/v1/[controller]/")]
     public class AdminController : Controller
     {
-        // GET: AdminController
-        public ActionResult Index()
+        [HttpPost(Name = "create-admin")]
+        public ActionResult<Admin> Post()
         {
             return View();
         }
 
-        // GET: AdminController/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Admin> GetById(int id)
         {
             return View();
         }
 
-        // GET: AdminController/Create
-        public ActionResult Create()
+        [HttpDelete]
+        public void Delete(int Id)
         {
-            return View();
+            
         }
 
-        // POST: AdminController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [HttpPut(Name = "update-admin")]
+        public void Update(Admin admin)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: AdminController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: AdminController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: AdminController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: AdminController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
