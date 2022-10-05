6020fa67b4445fc5e612722e96a25a5380c05597
Gregor Csatlós <g.csatlos1@gmail.com>
2022-10-05 22:35:36 +0200

    Adding auto generated files for Admin CRUD

---
+++ AppointmentSchedulerServer/Entities/Admin.cs
@@ -0,0 +1,16 @@
+﻿namespace AppointmentSchedulerServer.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Admin(int id, string name, string password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+    }
+}
