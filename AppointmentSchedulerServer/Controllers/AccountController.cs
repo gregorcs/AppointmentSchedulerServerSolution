f438075d340156f88b8e04e08c448e615466b418
Gregor Csatlós <g.csatlos1@gmail.com>
2022-10-25 19:54:47 +0200

    Adding jwt token handling to admin and user roles

--- AppointmentSchedulerServer/Controllers/AccountController.cs
+++ AppointmentSchedulerServer/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 ﻿using AppointmentSchedulerServer.Data_Transfer_Objects;
 using AppointmentSchedulerServer.Entities;
 using AppointmentSchedulerServer.Repositories;
+using AppointmentSchedulerServerTests.JWT;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppointmentSchedulerServer.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/v1/[controller]/")]
     public class AccountController : Controller
@@ -18,6 +21,7 @@ namespace AppointmentSchedulerServer.Controllers
 
         //TODO fix error not thrown when posting already registered email
         [HttpPost("create-account")]
+        [AllowAnonymous]
         public async Task<ActionResult<Account>> Post(Account account)
         {
             if (account is null)
@@ -30,14 +34,23 @@ namespace AppointmentSchedulerServer.Controllers
         }
         //error handling when calling repo - how should it look like?
         [HttpPost("authenticate")]
+        [AllowAnonymous]
         public async Task<IActionResult> Authenticate([FromBody] AccountDTO accountDTO)
         {
-            return await AccountRepository.ValidateAccountByEmailAndPassword(new Account(accountDTO)) 
-                ? Ok() : NotFound();
+            var isAuthenticated = await AccountRepository.ValidateAccountByEmailAndPassword(new Account(accountDTO));
+            if (isAuthenticated)
+            {
+                var token = JWTHandler.CreateUserToken(accountDTO);
+                return Ok(token);
+            } else
+            {
+                return BadRequest();
+            }
         }
 
         [HttpGet]
         [Route("{id:int}")]
+        [Authorize(Roles ="Admin")]
         public async Task<IActionResult> GetById(int id)
         {
             Account accountFound = await AccountRepository.FindById(id);
@@ -45,17 +58,20 @@ namespace AppointmentSchedulerServer.Controllers
         }
 
         [HttpDelete]
+        [Authorize(Roles = "User, Admin")]
         public void Delete(int Id)
         {
             
         }
 
         [HttpPut(Name = "update-account")]
+        [Authorize(Roles = "User, Admin")]
         public void Update(Account account)
         {
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> FindAll()
         {
             var result = await AccountRepository.FindAll();
--- AppointmentSchedulerServer/Program.cs
+++ AppointmentSchedulerServer/Program.cs
@@ -1,5 +1,11 @@
 using AppointmentSchedulerServer.DbConnections;
 using AppointmentSchedulerServer.Repositories;
+using AppointmentSchedulerServerTests.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -9,8 +15,29 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Authorization with bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(JWTHandler.jwtString)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
