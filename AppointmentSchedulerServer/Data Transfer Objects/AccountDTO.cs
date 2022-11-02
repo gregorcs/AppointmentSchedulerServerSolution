6df7fb4bdcbd5981eef12d9d1654989596be0b2b
Gregor Csatlós <g.csatlos1@gmail.com>
2022-11-02 21:46:45 +0100

    Bugfix making AccountDTO username nullable

--- AppointmentSchedulerServer/Data Transfer Objects/AccountDTO.cs
+++ AppointmentSchedulerServer/Data Transfer Objects/AccountDTO.cs
@@ -1,23 +1,28 @@
-﻿namespace AppointmentSchedulerServer.Data_Transfer_Objects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentSchedulerServer.Data_Transfer_Objects
 {
     public class AccountDTO
     {
+        public string? Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
         public AccountDTO(string username, string email, string password)
         {
-            Username = username;    
+            Username = username;
             Email = email;
             Password = password;
         }
-        public AccountDTO(string email)
+
+        public AccountDTO(string email, string password)
         {
             Email = email;
+            Password = password;
         }
+
         public AccountDTO()
         {
         }
-
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
